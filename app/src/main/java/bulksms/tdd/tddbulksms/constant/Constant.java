16d1d7ff4da1e1d38c4f579a2ad7a3d6ec5c85c0
mdyeasinali <yeasin15-8081@diu.edu.bd>
2017-05-08 15:01:44 +0600

    edit xml

--- app/src/main/java/bulksms/tdd/tddbulksms/constant/Constant.java
+++ app/src/main/java/bulksms/tdd/tddbulksms/constant/Constant.java
@@ -8,11 +8,11 @@ public class Constant {
     public static final String RECIVER_SMS_INFO_LIST = "receiver_sms_list";
     public static final String KEY_MESSAGE = "key_message";
 
-    public static final String GP = "GP";
+    public static final String GP = "GrameenPhone";
     public static final String ROBI = "ROBI";
-    public static final String BLINK = "BLINK";
+    public static final String BLINK = "BanglaLink ";
     public static final String AIRTEL = "AIRTEL";
-    public static final String TTALK = "TTALK";
+    public static final String TTALK = "Teletalk";
     public static final String ALL_PHONE = "ALL";
     public static final String EMULATOR = "EMULATOR";
     public static final String STATUS_NOT_DELIVERED = "0";
