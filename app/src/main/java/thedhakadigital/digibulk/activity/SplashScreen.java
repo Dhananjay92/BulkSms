09cb7475304442d2592b2b2fb172bba7df668272
yeahia <yeahia.arif@gmail.com>
2017-05-24 12:15:59 +0600

    "NOTIFICATION ADDED"

--- app/src/main/java/thedhakadigital/digibulk/activity/SplashScreen.java
+++ app/src/main/java/thedhakadigital/digibulk/activity/SplashScreen.java
@@ -1,29 +1,110 @@
 package thedhakadigital.digibulk.activity;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import thedhakadigital.digibulk.HelperClasses.Preferences;
+import thedhakadigital.digibulk.notification.Configuration;
+import thedhakadigital.digibulk.notification.DeleteTokenService;
+import thedhakadigital.digibulk.notification.NotificationUtils;
 import thedhakadigital.digibulk.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        Preferences preferences = Preferences.getInstance(this);
+        Activity activity = this;
+
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            int versionCode = packageInfo.versionCode;
+
+            if(preferences.getVersionCode()!=versionCode){
+
+                System.out.println("Version code changed");
+
+                Intent intentService = new Intent(activity, DeleteTokenService.class);
+                activity.startService(intentService);
+
+                preferences.setVersionCode(versionCode);
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                // checking for type intent filter
+                if (intent.getAction().equals(Configuration.REGISTRATION_COMPLETE)) {
+                    // fcm successfully registered
+                    // now subscribe to `global` topic to receive app wide notifications
+                    FirebaseMessaging.getInstance().subscribeToTopic(Configuration.TOPIC_GLOBAL);
+                }
+            }
+        };
+
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run(){
+                Bundle bundle = getIntent().getExtras();
+
+                if (bundle != null && bundle.get("data")!=null) {
+                    //here can get notification message
+                    String datas = bundle.get("data").toString();
+                    Toast.makeText(SplashScreen.this, datas, Toast.LENGTH_SHORT).show();
+                }
+
                 Intent intent = new Intent(getApplicationContext(), StartActivity.class);
                 startActivity(intent);
                 finish();
             }
         }, 2000);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // register GCM registration complete receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Configuration.REGISTRATION_COMPLETE));
+
+        // register new push postUrlFromNotification receiver
+        // by doing this, the activity will be notified each time a new postUrlFromNotification arrives
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Configuration.PUSH_NOTIFICATION));
+
+        // clear the notification area when the app is opened
+        NotificationUtils.clearNotifications(getApplicationContext());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
     }
 
 }
